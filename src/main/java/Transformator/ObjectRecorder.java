16622b2fc8d8c8c5ee1fca8f6d61f469ab0792f1
Oguz Demir <ogzdemr@gmail.com>
2018-02-26 01:13:24 -0600

    Object Recorder implementation

--- src/main/java/Transformator/ObjectRecorder.java
+++ src/main/java/Transformator/ObjectRecorder.java
@@ -1,26 +1,64 @@
 package Transformator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Created by od on 25.02.2018.
  */
 public class ObjectRecorder {
 
-    public ObjectRecorder instance;
+    private static FileOutputStream fileOutputStream;
+    private static ObjectOutputStream out;
+    private static int writeIndex = 1;
 
-    private ObjectRecorder(){
+    private static FileInputStream fileInputStream;
+    private static ObjectInputStream in;
+    private static int readIndex = 0;
 
+    public static void writeObject(Object object)  {
+        try {
+            if(out == null) {
+                fileOutputStream = new FileOutputStream(new File("out" + writeIndex));
+                out = new ObjectOutputStream(fileOutputStream);
+            }
+
+            out.writeObject(object);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public ObjectRecorder getInstance() {
-        if(instance == null) {
-            instance = new ObjectRecorder();
+    public static void finalizeWriting() {
+        try {
+            out.flush();
+            out.close();
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            out = null;
+            fileOutputStream = null;
+            writeIndex++;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return instance;
     }
 
     public static Object readObject(int index, String objectName) {
-        return null;
+        try {
+            if (index != readIndex) {
+                readIndex = index;
+                fileInputStream = new FileInputStream(new File("out" + index));
+                in = new ObjectInputStream(fileInputStream);
+            }
+
+            return in.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-
-
 }
