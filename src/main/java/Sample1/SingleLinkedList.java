5b580038f731a1dc81ec80058a4fa6581048d5a9
Oguz Demir <ogzdemr@gmail.com>
2018-02-26 00:37:14 -0600

    2 sample cases

---
+++ src/main/java/Sample1/SingleLinkedList.java
@@ -0,0 +1,31 @@
+package Sample1;
+
+import java.io.Serializable;
+
+/**
+ * Created by od on 11/19/17.
+ */
+public class SingleLinkedList implements Serializable {
+
+    Node header;
+    int size;
+
+    static class Node {
+
+        Node next;
+        int elem;
+    }
+
+    public void add(int x) {
+        Node n = new Node();
+        n.elem = x;
+        n.next = header;
+        header = n;
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+}
---
+++ src/main/java/Sample2/BankAccount.java
@@ -0,0 +1,24 @@
+package Sample2;
+
+import java.io.Serializable;
+
+/**
+ * Created by od on 25.02.2018.
+ */
+public class BankAccount implements Serializable {
+    public int accountId;
+    public String owner;
+    public int accountBalance;
+
+
+    public void addMoney(int amount) {
+        accountBalance += amount;
+    }
+
+    public void withdrawMoney(int amount) {
+        accountBalance -= amount;
+    }
+
+
+
+}
