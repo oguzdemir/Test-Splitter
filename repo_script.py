49e44890dd6b144aa9b777f38aea7606a3d0cef7
Oguz Demir <ogzdemr@gmail.com>
2018-10-27 17:24:22 -0500

    Changes for script

--- repo_script.py
+++ repo_script.py
@@ -2,25 +2,31 @@ import subprocess
 import os
 import shutil
 import sys
-
+import time
 
 splitter = "git@github.com:oguzdemir/Test-Splitter.git"
 inst_path = "repositories"
 
-
 repos = [
-    "git@github.com:apache/commons-lang.git",
-    "git@github.com:jfree/jfreechart.git",
-]
-
+    'https://github.com/CrawlScript/WebCollector', 'https://github.com/spring-projects/spring-boot',
+    'https://github.com/kevinsawicki/http-request', 'https://github.com/dropwizard/dropwizard',
+    'https://github.com/TooTallNate/Java-WebSocket', 'https://github.com/apache/struts',
+    'https://github.com/thinkaurelius/titan', 'https://github.com/oryxproject/oryx',
+    'https://github.com/Activiti/Activiti', 'https://github.com/apache/hadoop',
+    'https://github.com/apache/jackrabbit-oak', 'https://github.com/square/retrofit',
+    'https://github.com/alibaba/fastjson', 'https://github.com/doanduyhai/achilles', 'https://github.com/apache/flink',
+    'https://github.com/wro4j/wro4j', 'https://github.com/wildfly/wildfly', 'https://github.com/knightliao/disconf',
+    'https://github.com/addthis/stream-lib', 'https://github.com/looly/hutool', 'https://github.com/jfree/jfreechart',
+    'https://github.com/orbit/orbit', 'https://github.com/elasticjob/elastic-job-lite',
+    'https://github.com/apache/incubator-dubbo', 'https://github.com/spotify/helios', 'URL',
+    'https://github.com/undertow-io/undertow', 'https://github.com/apache/hbase']
 
 dependency_xml = [
-    "<dependency>", "\t<groupId>od</groupId>", "\t<artifactId>Test-Splitter</artifactId>",
-    "\t<version>1.0-SNAPSHOT</version>","\t<scope>system</scope>",
-    "\t<systemPath>${project.basedir}/../../Test-Splitter-1.0-SNAPSHOT-jar-with-dependencies.jar</systemPath>",
-    "</dependency>"
+"<dependency>", "\t<groupId>od</groupId>", "\t<artifactId>Test-Splitter</artifactId>",
+"\t<version>1.0-SNAPSHOT</version>", "\t<scope>system</scope>",
+"\t<systemPath>${project.basedir}/../../Test-Splitter-1.0-SNAPSHOT-jar-with-dependencies.jar</systemPath>",
+"</dependency>"
 ]
-
 
 def construct_xml(indent):
     ind_string = ' ' * indent + '\t'
@@ -30,8 +36,8 @@ def construct_xml(indent):
     return s
 
 
-def run_something(*args, cwd="./", file_name = None):
-    result = subprocess.run(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=cwd )
+def run_something(*args, cwd="./", file_name=None):
+    result = subprocess.run(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=cwd)
 
     if file_name is not None:
         with open(file_name, 'w+') as content_file:
@@ -39,21 +45,17 @@ def run_something(*args, cwd="./", file_name = None):
             content_file.write(result.stdout.decode("utf-8"))
             content_file.close()
 
+    time.sleep(1)
+
 
 def to_data(out_string):
     data = [0, 0, 0, 0]
-
-    for sx in out_string.splitlines():
-        if "Tests run: " in sx:
-            infos = sx.split(" ")
-            if "elapsed:" in infos:
-                data[0] += int(infos[infos.index("run:") + 1].replace(',', ''))
-                data[1] += int(infos[infos.index("Failures:") + 1].replace(',', ''))
-                data[2] += int(infos[infos.index("Errors:") + 1].replace(',', ''))
-                data[3] += float(infos[infos.index("elapsed:") + 1])
-
-    res = str(data)
-    return res[1:len(res)-1]
+    infos = out_string.split(" ")
+    data[0] = int(infos[infos.index("run:") + 1].replace(',', ''))
+    data[1] = int(infos[infos.index("Failures:") + 1].replace(',', ''))
+    data[2] = int(infos[infos.index("Errors:") + 1].replace(',', ''))
+    data[3] = float(infos[infos.index("elapsed:") + 1])
+    return data
 
 
 def find_instrumentation_time(file_name):
@@ -62,7 +64,7 @@ def find_instrumentation_time(file_name):
         while line:
             if "Instrumentation took: " in line:
                 temp = line.rstrip("\n").split(" ")
-                return temp[temp.index("Instrumentation took: ") + 1]
+                return temp[temp.index("took:") + 1]
             line = fp.readline()
 
 
@@ -76,7 +78,7 @@ def file_to_map(file_name):
         while line:
             if "Running " in line:
                 temp = line.rstrip("\n").split(" ")
-                full_name = temp[temp.index("Running ") + 1]
+                full_name = temp[temp.index("Running") + 1]
             if "Tests run: " in line and ", Time elapsed: " in line:
                 data = to_data(line.rstrip("\n"))
                 *others, partial_name = full_name.split(".")
@@ -85,9 +87,7 @@ def file_to_map(file_name):
                     res[class_name] = [sum(x) for x in zip(res[class_name], data)]
                 else:
                     res[class_name] = data
-                    res[class_name] = [int(info[2].rstrip(",")), int(info[4].rstrip(",")), int(info[6].rstrip(",")),
-                                       float(info[11].rstrip(","))]
-                entire_data = res[class_name] = [sum(x) for x in zip(entire_data, data)]
+                entire_data = [sum(x) for x in zip(entire_data, data)]
             line = fp.readline()
 
     return res, entire_data
@@ -99,11 +99,11 @@ def change_pom(file_path):
         content = content_file.read()
         ind = content.find("<dependencies>", index)
         while ind > 0:
-            ind2 = ind -1
+            ind2 = ind - 1
             while content[ind2] != '\n':
                 ind2 = ind2 - 1
 
-            content = content[:ind + 14] + construct_xml(ind - ind2) + content[ind+14:]
+            content = content[:ind + 14] + construct_xml(ind - ind2) + content[ind + 14:]
             ind = content.find("<dependencies>", ind + 15)
 
     with open(file_path, 'w') as content_file:
@@ -124,7 +124,7 @@ def go_single_repo(address):
     shutil.copytree(repo + "/" + repo, repo + "/" + repo + "_splitted")
     os.chdir(repo)
     run_something("java", "-jar", "Test-Splitter-1.0-SNAPSHOT-jar-with-dependencies.jar", "-a",
-                 "-p", "./" + repo + "/" + repo, cwd="../", file_name="instrumentation.txt")
+                  "-p", "./" + repo + "/" + repo, cwd="../", file_name="instrumentation.txt")
     run_something("mvn", "test", cwd="./" + repo + "_original", file_name="original.txt")
     run_something("mvn", "test", cwd="./" + repo, file_name="generation.txt")
     try:
@@ -137,12 +137,13 @@ def go_single_repo(address):
     generation, generation_data = file_to_map("generation.txt")
     splitted, splitted_data = file_to_map("splitted.txt")
 
-
     with open("./module_results.csv", 'w+') as fp:
         fp.write(
             "Module Name, Original Tests, Generated Tests, Errors, Failures, Elapsed Time, Time For Snapshots, Original Elapsed Time\n")
         for d in splitted:
-            fp.write("%s,%d,%d,%d,%d,%f,%f,%f\n" % (d, original[d][0], splitted[d][0], splitted[d][1], splitted[d][2], splitted[d][3], generation[d][3], original[d][3]))
+            fp.write("%s,%d,%d,%d,%d,%f,%f,%f\n" % (
+            d, original[d][0], splitted[d][0], splitted[d][1], splitted[d][2], splitted[d][3], generation[d][3],
+            original[d][3]))
 
     os.chdir("../")
 
@@ -157,13 +158,23 @@ def main():
     #     exit(1)
     # address = sys.argv[1]
 
-    run_something("git", "clone", splitter)
     shutil.rmtree(inst_path, ignore_errors=True)
     os.mkdir(inst_path)
-    shutil.copy("./Test-Splitter/jar/Test-Splitter-1.0-SNAPSHOT-jar-with-dependencies.jar", inst_path)
     os.chdir(inst_path)
-    for x in range(len(repos)):
-        go_single_repo(repos[x])
+    run_something("git", "clone", splitter)
+    shutil.copy("./Test-Splitter/jar/Test-Splitter-1.0-SNAPSHOT-jar-with-dependencies.jar", "./")
+
+    with open("./allRes1.csv", 'w+') as result_file:
+        result_file.write(
+            "Repo, Original Tests, Generated Tests, Errors, Failures, Elapsed Time, Time For Snapshots, Original Elapsed Time\n")
+        for x in range(len(repos)):
+            try:
+                all_data = go_single_repo(repos[x])
+            except Exception:
+                continue
+            repo_name = repos[x][repos[x].rfind("/") + 1:repos[x].find(".git")]
+            result_file.write("%s,%d,%d,%d,%d,%f,%f,%f\n" % (
+            repo_name, all_data[0], all_data[8], all_data[9], all_data[10], all_data[3], all_data[7], all_data[11]))
 
 
 if __name__ == "__main__":
