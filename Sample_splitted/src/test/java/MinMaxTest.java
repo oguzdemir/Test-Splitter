6b6db80022445f3b858ff12e1e94358d50c04bd4
Oguz Demir <ogzdemr@gmail.com>
2018-11-13 23:40:38 -0600

    Updating sample

--- Sample_splitted/src/test/java/MinMaxTest.java
+++ Sample_splitted/src/test/java/MinMaxTest.java
@@ -31,126 +31,62 @@ public class MinMaxTest {
         int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 1, 1);
         int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
         System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU4", 0, 0), MinMax.min(x, y));
     }
 
     @Test
     public void generatedU5() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
+        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 1, 1);
+        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
         System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU5", 0, 0), MinMax.min(x, y));
     }
 
     @Test
     public void generatedU6() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
+        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 1, 0);
+        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
         System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU6", 0, 0), MinMax.min(x, y));
     }
 
     @Test
     public void generatedU7() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 1, 1);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
+        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 1, 0);
+        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
         System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU7", 0, 0), MinMax.min(x, y));
     }
 
     @Test
     public void generatedU8() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
+        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 2, 1);
+        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
+        System.out.println("Testing max");
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU8", 0, 0), MinMax.max(x, y));
     }
 
     @Test
     public void generatedU9() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
+        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 2, 1);
         int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
+        System.out.println("Testing max");
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU9", 0, 0), MinMax.max(x, y));
     }
 
     @Test
     public void generatedU10() {
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 1, 0);
+        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 2, 0);
         int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
+        System.out.println("Testing max");
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU10", 0, 0), MinMax.max(x, y));
     }
 
     @Test
     public void generatedU11() {
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 1, 0);
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        System.out.println("Testing min");
-        assertEquals(1, MinMax.min(x, y));
-    }
-
-    @Test
-    public void generatedU12() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 2, 1);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
-    }
-
-    @Test
-    public void generatedU13() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
-    }
-
-    @Test
-    public void generatedU14() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
-    }
-
-    @Test
-    public void generatedU15() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 2, 1);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
-    }
-
-    @Test
-    public void generatedU16() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
-    }
-
-    @Test
-    public void generatedU17() {
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
-        System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
-    }
-
-    @Test
-    public void generatedU18() {
-        int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 2, 0);
-        int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 0);
-        System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
-    }
-
-    @Test
-    public void generatedU19() {
         int x = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_minMaxTest1", 2, 0);
         int y = (Integer) com.od.TestSplitter.Transformator.ObjectRecorder.readSpecificObject("java.lang.Integer", 1);
         System.out.println("Testing max");
-        assertEquals(2, MinMax.max(x, y));
+        assertEquals(com.od.TestSplitter.Transformator.ObjectRecorder.readObject("MinMaxTest_generatedU11", 0, 0), MinMax.max(x, y));
     }
 }
